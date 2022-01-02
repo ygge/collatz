663f628bf43ec83c9a32e222eaf6f7562f3e86a2
Henrik Ygge <henrik.ygge@ericsson.com>
2022-01-02 17:25:15 +0100

    solved for 10^8

--- src/main/java/Collatz.java
+++ src/main/java/Collatz.java
@@ -5,6 +5,9 @@ import java.util.Set;
 
 public class Collatz {
 
+    private static final long MAX = 100000000L;
+    private static final long STORE_MAX = 10000000L;
+
     public static void main(String[] args) {
         Map<Long, Result> cache = new HashMap<>();
         Set<Long> seen = new HashSet<>();
@@ -15,13 +18,14 @@ public class Collatz {
         long maxHeight = 0;
         long stepsIndex = 0;
         long heightIndex = 0;
-        int len = 1;
-        for (long i = 2; i <= 10000000; ++i) {
-            int len2 = Long.toString(i).length();
-            if (len2 > len) {
-                System.out.printf(" <%9d %8d %5d %9d %15d%n", i, stepsIndex, maxSteps, heightIndex, maxHeight);
+        int goal = 10;
+        long start = System.nanoTime();
+        for (long i = 2; i <= MAX; ++i) {
+            if (i == goal) {
+                System.out.printf(" <%9d %8d %5d %9d %15d in %d ms%n", i, stepsIndex, maxSteps, heightIndex, maxHeight, (System.nanoTime()-start)/1000000);
                 maxSteps = maxHeight = stepsIndex = heightIndex = 0;
-                len = len2;
+                goal *= 10;
+                start = System.nanoTime();
             }
             var result = calc(i, cache, seen);
             if (result.steps > maxSteps) {
@@ -39,12 +43,14 @@ public class Collatz {
         if (cache.containsKey(i)) {
             return cache.get(i);
         }
-        if (!seen.add(i)) {
+        if (i <= STORE_MAX && !seen.add(i)) {
             throw new IllegalStateException(String.format("Loop detected for i=%d", i));
         }
         var ii = (i&1) == 1 ? 3*i+1 : i/2;
         var result = calc(ii, cache, seen).update(i);
-        cache.put(i, result);
+        if (i <= STORE_MAX) {
+            cache.put(i, result);
+        }
         return result;
     }
 
